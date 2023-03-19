640e66de3d89b57a0a7e8d770d03a9fe9e5c30a7
Raúl España <uo278036@uniovi.es>
2023-03-19 15:24:33 +0100

    SDI-2223-107-7.3-División de las rutas en módulos, peticiones GET y parámetros.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -1,3 +1,6 @@
+let app = express();
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,17 +9,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+require("./routes/songs.js")(app)
 var app = express();
 
-pp.get("/songs", function (req, res) {
-  console.log("depurar aqui")
-  res.send("Lista de canciones")
-});
-app.get("/singers", function (req, res) {
-  console.log("depurar aqui")
-  res.send("Lista de cantantes")
-});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
