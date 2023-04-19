2f46eb6d8384fd427ed3acf6456589d7f3e3feb6
Chacal <uo278036@uniovi.es>
2023-04-19 11:03:04 +0200

    SDI-2223-107-Ejercicio-complementario1.

--- musicstoreapp/routes/api/songsAPIv1.0.js
+++ musicstoreapp/routes/api/songsAPIv1.0.js
@@ -11,6 +11,7 @@ module.exports = function (app, songsRepository, usersRepository) {
             res.json({ error: "Se ha producido un error al recuperar las canciones." })
         });
     });
+
     app.post('/api/v1.0/songs', function (req, res) {
         try {
             let song = {
@@ -20,18 +21,29 @@ module.exports = function (app, songsRepository, usersRepository) {
                 author: req.session.user
             }
             // Validar aquí: título, género, precio y autor.
-            songsRepository.insertSong(song, function (songId) {
-                if (songId === null) {
-                    res.status(409);
-                    res.json({error: "No se ha podido crear la canción. El recurso ya existe."});
+
+            validate(song, function (errors){
+                if (errors !== null && errors.length > 0){
+                    res.status(422); //entidad no pocesable
+                    res.json({errors: errors});
                 } else {
-                    res.status(201);
-                    res.json({
-                        message: "Canción añadida correctamente.",
-                        _id: songId
-                    })
+                    songsRepository.insertSong(song, function (songId) {
+                        if (songId === null) {
+                            let errors = new Array()
+                            res.status(409);
+                            errors.push("No se ha podido crear la canción. El recurso ya existe.")
+                            res.json({errors: errors});
+                        } else {
+                            res.status(201);
+                            res.json({ message: "Canción añadida correctamente.",
+                                _id: songId
+                            })
+                        }
+                    });
                 }
-            });
+            })
+
+
         } catch (e) {
             res.status(500);
             res.json({error: "Se ha producido un error al intentar crear la canción: " + e})
@@ -60,111 +72,188 @@ module.exports = function (app, songsRepository, usersRepository) {
             res.json({error: "Se ha producido un error :" + e})
         }
     });
-    app.delete('/api/v1.0/songs/:id', function (req, res) {
-        try {
-            let songId = ObjectId(req.params.id)
-            let filter = {_id: songId}
-            songsRepository.deleteSong(filter, {}).then(result => {
-                if (result === null || result.deletedCount === 0) {
-                    res.status(404);
-                    res.json({error: "ID inválido o no existe, no se ha borrado el registro."});
-                } else {
-                    res.status(200);
-                    res.send(JSON.stringify(result));
-                }
-            }).catch(error => {
-                res.status(500);
-                res.json({error: "Se ha producido un error al eliminar la canción."})
-            });
-        } catch (e) {
-            res.status(500);
-            res.json({error: "Se ha producido un error, revise que el ID sea válido."})
-        }
-    });
+
     app.put('/api/v1.0/songs/:id', function (req, res) {
         try {
             let songId = ObjectId(req.params.id);
             let filter = {_id: songId};
             //Si la _id NO no existe, no crea un nuevo documento.
             const options = {upsert: false};
-            let song = {
-                author: req.session.user
-            }
+            let song = { author: req.session.user }
             if (typeof req.body.title != "undefined" && req.body.title != null)
                 song.title = req.body.title;
             if (typeof req.body.kind != "undefined" && req.body.kind != null)
                 song.kind = req.body.kind;
             if (typeof req.body.price != "undefined" && req.body.price != null)
                 song.price = req.body.price;
-            songsRepository.updateSong(song, filter, options).then(result => {
-                if (result === null) {
-                    res.status(404);
-                    res.json({error: "ID inválido o no existe, no se ha actualizado la canción."});
-                }
-                //La _id No existe o los datos enviados no difieren de los ya almacenados.
-                else if (result.modifiedCount == 0) {
-                    res.status(409);
-                    res.json({error: "No se ha modificado ninguna canción."});
-                }
-                else{
-                    res.status(200);
-                    res.json({
-                        message: "Canción modificada correctamente.",
-                        result: result
+
+            isAuthor(song.author, songId, function (isAuthor){ //en validador como un custom
+                if (isAuthor){
+                    validate(song, function(errors){
+                        if (errors == null || errors.length == 0) { //no errors
+                            songsRepository.updateSong(song, filter, options).then(result => {
+                                if (result === null) {
+                                    res.status(404);
+                                    res.json({error: "ID inválido o no existe, no se ha actualizado la canción."});
+                                }
+                                //La _id No existe o los datos enviados no difieren de los ya almacenados.
+                                else if (result.modifiedCount == 0) {
+                                    res.status(409);
+                                    res.json({error: "No se ha modificado ninguna canción."});
+                                } else{
+                                    res.status(200);
+                                    res.json({
+                                        message: "Canción modificada correctamente.",
+                                        result: result
+                                    })
+                                }
+                            }).catch(error => {
+                                res.status(500);
+                                res.json({error : "Se ha producido un error al modificar la canción."})
+                            });
+                        } else {
+                            res.status(403);
+                            res.json({error: errors})
+                        }
                     })
+                } else{
+                    res.status(500);
+                    res.json({error:"El usuario no es autor de la canción que se está intentando modificar"})
                 }
-            }).catch(error => {
-                res.status(500);
-                res.json({error : "Se ha producido un error al modificar la canción."})
-            });
+            })
         } catch (e) {
             res.status(500);
             res.json({error: "Se ha producido un error al intentar modificar la canción: "+ e})
         }
     });
-    app.post('/api/v1.0/users/login', function (req, res) {
+
+    app.delete('/api/v1.0/songs/:id', function (req, res) {
         try {
-            let securePwd = app.get('crypto').createHmac('sha256',
-                app.get('clave'))
-                .update(req.body.password)
-                .digest('hex');
+            let songId = ObjectId(req.params.id)
+            let filter = {_id: songId}
+
+            isAuthor(req.session.user, songId, function (isAuthor){
+                if (isAuthor){
+                    songsRepository.deleteSong(filter, {}).then(result => {
+                        if (result === null || result.deletedCount === 0) {
+                            res.status(404);
+                            res.json({error: "ID inválido o no existe, no se ha borrado el registro."});
+                        } else {
+                            res.status(200);
+                            res.send(JSON.stringify(result));
+                        }
+                    }).catch(error => {
+                        res.status(500);
+                        res.json({error: "Se ha producido un error al eliminar la canción."})
+                    });
+                } else {
+                    res.status(500);
+                    res.json({error: "El usuario no es el autor de la canción que se intenta eliminar."})
+                }
+            })
+
+        } catch (e) {
+            res.status(500);
+            res.json({error: "Se ha producido un error, revise que el ID sea válido."})
+        }
+    });
+
+    app.post('/api/v1.0/users/login', function(req, res){
+        try{
+            let securePassword = app.get("crypto").createHmac('sha256', app.get('clave')).update(req.body.password).digest('hex');
             let filter = {
                 email: req.body.email,
-                password: securePwd
-            };
+                password: securePassword
+            }
             let options = {};
             usersRepository.findUser(filter, options).then(user => {
                 if (user == null){
-                    res.status(401); //Unauthorized
+                    res.status(401); //unauthorized
                     res.json({
                         message: "usuario no autorizado",
                         authenticated: false
-                    });
+                    })
                 } else {
                     let token = app.get('jwt').sign(
-                        {user: user.email, time:Date.now() / 1000},
+                        {user: user.email, time:Date.now()/1000},
                         "secreto"
                     );
                     res.status(200);
                     res.json({
                         message: "usuario autorizado",
-                        authenticated: true,
+                        authenticated : true,
                         token: token
-                    });
+                    })
                 }
             }).catch(error => {
                 res.status(401);
                 res.json({
-                    message: "Se ha  producido un error al verificar las credenciales",
-                    authenticated: false
-                });
+                    message: "Se ha producido un error al verificar credenciales",
+                    authenticated : false
+                })
             })
-        } catch (e) {
-            res.status(500); //server error
+        } catch (e){
+            res.status(500);
             res.json({
-                message: "Se ha producido un error al verificar las credenciales",
+                message: "Se ha producido un error al cerificar credenciales",
                 authenticated: false
+            })
+        }
+    });
+
+
+    //AUX
+    function isAuthor(user, songId, callback) {
+        let filter = {$and: [{'_id': songId}, {'author': user}]};
+        let options ={};
+        songsRepository.getSongs(filter, options).then(songs => {
+            if (songs==null || songs.length <= 0){
+                callback(false);
+            }
+            else
+                callback(true);
+        })
+
+    }
+
+    function validate(song, callback) {
+        let errors = new Array();
+
+        //PRICE : positive
+        if (song.price === null || typeof song.price === 'undefined' || song.price < 0 || song.price === '') {
+            errors.push({
+                "value": "price",
+                "msg": "El precio no es valido",
+                "param": "price",
+                "location": "body"
             });
         }
-    })
+
+        //TITLE :
+        if (song.title === null || typeof song.title === 'undefined' || song.title === '' || song.title.trim() == 0) {
+            errors.push({
+                "value": "title",
+                "msg": "El title no es valido",
+                "param": "title",
+                "location": "body"
+            });
+        }
+
+        //GENERO
+        if (song.kind === null || typeof song.kind === 'undefined' || song.kind === '') {
+            errors.push({
+                "value": "kind",
+                "msg": "El genero no es valido",
+                "param": "kind",
+                "location": "body"
+            });
+        }
+
+        if (errors.length > 0) {
+            callback(errors);
+        } else {
+            callback(null);
+        }
+
+    }
 }
